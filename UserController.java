dc1108953bc5189a2893b620318f010037e55e2a
KiBok-Yu <kibokyu@hanmail.net>
2020-02-12 17:24:05 +0900

    Add UserController[insert]

--- UserController.java
+++ UserController.java
@@ -7,4 +7,11 @@ public class UserController{
     logger.info("경로 :login");
     return "user/login";
    }
+
+ //회원가입 페이지로 이동
+@RequestMapping("/user/insert.do")
+ public String insert(){
+    logger.info("경로 :insert");
+    return "user/insert";
+   }
 }
